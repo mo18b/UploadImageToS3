dcb748fab503610ec2f8911f9b953488ff1871e9
MCL <modupe18ab@gmail.com>
2025-04-12 00:40:03 -0500

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = 'https://g02a7ip9b0.execute-api.us-east-2.amazonaws.com/default/myfunctionstrike'
+const API_ENDPOINT = 'https://nqo7uv8i7b.execute-api.us-east-1.amazonaws.com/default/uploadtos3serverless'
 
 export default {
   name: 'app',
